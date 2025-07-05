fc2a360b88c8752de6f2e6fd7208aff09b422f09
Gregor Wünsch <41543402+gwuen@users.noreply.github.com>
2025-07-06 01:30:50 +0200

    Bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let toSort = arr.length; toSort > 1; toSort--) {
+        for (let i = 0; i < toSort - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                const tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+            }
+        }
+    }
 }
