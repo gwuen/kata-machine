72338d1916ab9bec18b4e22d997ac0688befb1f5
Gregor Wünsch <41543402+gwuen@users.noreply.github.com>
2025-07-05 23:39:18 +0200

    Two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpLen = Math.floor(Math.sqrt(breaks.length));
 
+    let i;
+    for (i = jumpLen; i < breaks.length; i += jumpLen) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    for (let j = i - jumpLen; j <= i; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+
+    return -1;
 }
