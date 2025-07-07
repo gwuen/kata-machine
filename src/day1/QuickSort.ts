7fcfc8b5e175a2fd22ae90cf4d229bd7e063d2fd
Gregor Wünsch <41543402+gwuen@users.noreply.github.com>
2025-07-07 14:17:30 +0200

    Quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,28 @@
 export default function quick_sort(arr: number[]): void {
+    // base case (nothing to sort)
+    if (arr.length <= 1) {
+        return;
+    }
 
+    const pivotIdx = Math.floor(arr.length / 2);
+    const pivot = arr[pivotIdx];
+
+    const smaller = [];
+    const greater = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        // don't compare pivot to itself
+        if (i === pivotIdx) continue;
+
+        if (arr[i] <= pivot) {
+            smaller.push(arr[i]);
+        } else {
+            greater.push(arr[i]);
+        }
+    }
+
+    quick_sort(smaller);
+    quick_sort(greater);
+
+    arr.splice(0, arr.length, ...smaller, pivot, ...greater);
 }
