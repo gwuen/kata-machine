6deed225fe472bec8ad3fe0b31d6acd9570bf94e
Gregor Wünsch <41543402+gwuen@users.noreply.github.com>
2025-07-08 13:37:37 +0200

    Binary tree breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,24 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: BinaryNode<number>[] = [head];
 
+    while (q.length) {
+        // dequeue first node in queue
+        const curr = q.shift() as BinaryNode<number>;
+
+        if (curr.value === needle) {
+            return true;
+        }
+
+        // enqueue left child
+        if (curr.left) {
+            q.push(curr.left);
+        }
+
+        // enqueue right child
+        if (curr.right) {
+            q.push(curr.right);
+        }
+    }
+
+    return false;
 }
