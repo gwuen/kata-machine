24b5fb8443533f8364969fd5f02159d50d6ff2e8
Gregor Wünsch <41543402+gwuen@users.noreply.github.com>
2025-07-08 14:10:08 +0200

    Compare binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,18 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    // both nodes do not exist
+    if (a === null && b === null) {
+        return true;
+    }
 
+    // only one node does not exist
+    if (a === null || b === null) {
+        return false;
+    }
+
+    // the node's values are not equal
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
